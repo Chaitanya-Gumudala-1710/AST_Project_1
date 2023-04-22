3cb3c65465f1cd86104e41ce39bb2d866ca8054f
Chaitanya-Gumudala-1710 <84703770+Chaitanya-Gumudala-1710@users.noreply.github.com>
2023-04-22 18:24:34 +0200

    Update Project.py

--- Order_Management_System/Project.py
+++ Order_Management_System/Project.py
@@ -1,4 +1,4 @@
-# Create class of Order, Order Detail, Items, Stare - Chaitanya
+# Create class of Order, Order Detail, Items, State - Chaitanya
 # Create class of Customer, Address, Contract Details - Anudeep
 # Create class of Payment, Cash, Bank Transfers, Credit Card - Kamendu
 # Test the code - Jay
