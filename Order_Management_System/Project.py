101313479b2337c31632fd0ac6a73ce2ab9923c2
Chaitanya <chaitanyagumudala961@gmail.com>
2023-04-22 18:45:11 +0200

    Created a Class State

--- Order_Management_System/Project.py
+++ Order_Management_System/Project.py
@@ -1,4 +1,13 @@
 # Create class of Order, Order Detail, Items, State - Chaitanya
+# Creating a class named "State" to represent different states of an order
+class State:
+    PENDING = "Order is Pending"
+    ACCEPTED = "Order is Accepted"
+    REJECTED = "Order is Rejected"
+    PROCESSING = "Order is Processing"
+    SHIPPING = "Order is Shipping"
+    CLOSED = "Order is Closed"
+
 # Create class of Customer, Address, Contract Details - Anudeep
 # Create class of Payment, Cash, Bank Transfers, Credit Card - Kamendu
 # Test the code - Jay
